9f9c1b82e5445719efd0c027e7cd3ddf027adb8c
Yves Casio <yveschristianpcasio@gmail.com>
2026-01-08 04:09:42 +0800

    feat: API for GIPHY

---
+++ src/api/giphyApi.js
@@ -0,0 +1,19 @@
+const API_KEY = process.env.GIPHY_API_KEY;
+
+const getGifImage = async function (searchQuery) {
+	try {
+		const res = await fetch(
+			`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${searchQuery}`
+		);
+
+		if (!res.ok) throw new Error(`Failed to fetch image: ${res}`);
+
+		const data = await res.json();
+
+		console.log(data);
+
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./assets/styles/light.css";
 import "./index.css";
 
+import "./api/giphyApi.js";
 import "./api/weatherApi.js";
 
 console.log("Template is working");
