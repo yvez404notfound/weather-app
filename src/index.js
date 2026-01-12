4d4875b7708141b157e0b57cd2f60b1e2c8a6c9f
Yves Casio <yveschristianpcasio@gmail.com>
2026-01-12 22:16:20 +0800

    feat: HTML layout and Dynamic color theme generator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import "./assets/styles/light.css";
+import "./assets/styles/modern-normalize.css";
 import "./index.css";
+
+/* Color Schemes */
+import "./assets/styles/clear-dark.css";
+import "./assets/styles/clear-day.css";
+/* ============= */
 
 import "./api/giphyApi.js";
 import "./api/weatherApi.js";
+
+import DynamicColorThemeGenerator from "./services/dynamicColorThemeGenerator.js";
+
+const bodyElement = document.querySelector("body");
+
+const dynamicColorThemeGenerator = new DynamicColorThemeGenerator(bodyElement);
+dynamicColorThemeGenerator.initDefaultTheme();
 
 console.log("Template is working");
